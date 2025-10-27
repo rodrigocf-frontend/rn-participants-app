9eb3d7c35f471f644731ce94c916a077de083f36
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2025-10-27 20:04:26 -0300

    chore: adjust theme project

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,14 +1,13 @@
-export default {
+import { DefaultTheme } from "styled-components/native";
+
+export const theme: DefaultTheme = {
   color: {
     WHITE: "#FFFFFF",
-
     GRAY_100: "#FDFCFE",
     GRAY_200: "#6B6B6B",
-    GRAY_800: "#1F1E25",
-    GRAY_900: "#131016",
-
+    GRAY_300: "#1F1E25",
+    GRAY_400: "#131016",
     RED_900: "#E23C44",
-
     GREEN_900: "#31CF67",
   },
   fontFamily: {
@@ -16,6 +15,7 @@ export default {
     ROBOTO_BOLD: "Roboto_700Bold",
   },
   fontSize: {
+    XSM: "10px",
     SM: "14px",
     NM: "16px",
     MD: "20px",
