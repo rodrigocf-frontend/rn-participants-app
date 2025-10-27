9b8a19956b5b25d117641789122919b5ca29d3db
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2025-10-27 20:05:24 -0300

    feat: create  events home route layout

--- src/screen/Home/index.tsx
+++ src/screen/Home/index.tsx
@@ -1,9 +1,77 @@
-import { Text, View } from "react-native";
+import { InputText } from "../../components/InputText";
+import { Button } from "../../components/Button";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { useTheme } from "styled-components/native";
+import { Highlight } from "../../components/Highlight";
+import { ListItem } from "../../components/ListItem";
+import {
+  Container,
+  DateContainer,
+  Title,
+  TitleContainer,
+  WrapperInput,
+} from "./styles";
+import { FlatList } from "react-native";
+import { InputDate } from "../../components/InputDate";
+import { useNavigation } from "@react-navigation/native";
+import { ListEmpty } from "../../components/ListEmpty";
+
+type Event = {
+  id: string;
+  name: string;
+  date: string;
+};
+
+const participants: Event[] = [
+  // { id: "1", name: "Evento 1", date: "14/02/2025" },
+  // { id: "2", name: "Evento 1", date: "14/02/2025" },
+  // { id: "3", name: "Evento 1", date: "14/02/2025" },
+  // { id: "4", name: "Evento 1", date: "14/02/2025" },
+  // { id: "5", name: "Evento 1", date: "14/02/2025" },
+  // { id: "6", name: "Evento 1", date: "14/02/2025" },
+  // { id: "7", name: "Evento 1", date: "14/02/2025" },
+  // { id: "8", name: "Evento 1", date: "14/02/2025" },
+  // { id: "9", name: "Evento 1", date: "14/02/2025" },
+];
 
 export function Home() {
+  const theme = useTheme();
+
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <Container>
+      <Highlight title="Cadastrar evento" />
+      <WrapperInput>
+        <InputText placeholder="Nome do evento" />
+        <Button variant="GREEN_900">
+          <MaterialIcons name="add" size={24} color={theme.color.WHITE} />
+        </Button>
+      </WrapperInput>
+      <DateContainer>
+        <InputDate />
+      </DateContainer>
+      <TitleContainer>
+        <Title>Eventos</Title>
+      </TitleContainer>
+      <FlatList
+        data={participants}
+        keyExtractor={({ id }) => id}
+        renderItem={({ item }) => (
+          <ListItem
+            onPress={() => navigation.navigate("Participants")}
+            title={item.name}
+            subtitle={item.date}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={
+          <ListEmpty
+            text="Nenhum evento criado ainda? 
+Adicione eventos a sua lista."
+          />
+        }
+      />
+    </Container>
   );
 }
---
+++ src/screen/Home/styles.ts
@@ -0,0 +1,30 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import styled from "styled-components/native";
+
+export const Container = styled(SafeAreaView)`
+  flex: 1;
+  background-color: ${({ theme }) => theme.color.GRAY_400};
+  padding: 24px 24px;
+`;
+
+export const WrapperInput = styled.View`
+  flex-direction: row;
+  align-self: stretch;
+  gap: 7px;
+  margin: 34px 0 16px 0;
+`;
+
+export const DateContainer = styled.View`
+  align-items: center;
+  margin-right: auto;
+`;
+
+export const TitleContainer = styled.View`
+  margin: 42px 0 16px 0;
+`;
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.color.GRAY_100};
+  font-family: ${({ theme }) => theme.fontFamily.ROBOTO_BOLD};
+  font-size: ${({ theme }) => theme.fontSize.MD};
+`;
