6b6689f1a6c2edb6a8aec9f08571d1fd4e6be85f
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2025-10-27 20:05:04 -0300

    feat: create layout participants route

--- src/screen/Participants/index.tsx
+++ src/screen/Participants/index.tsx
@@ -1,9 +1,60 @@
-import { Text, View } from "react-native";
+import { InputText } from "../../components/InputText";
+import { Button } from "../../components/Button";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { useTheme } from "styled-components/native";
+import { Highlight } from "../../components/Highlight";
+import { ListItem } from "../../components/ListItem";
+import { Container, Title, TitleContainer, WrapperInput } from "./styles";
+import { FlatList } from "react-native";
+import { ListEmpty } from "../../components/ListEmpty";
+
+type Participant = {
+  id: string;
+  name: string;
+};
+
+const participants: Participant[] = [
+  { id: "1", name: "Rodolfo Gonçalves" },
+  { id: "2", name: "Rodolfo Gonçalves" },
+  { id: "3", name: "Rodolfo Gonçalves" },
+  { id: "4", name: "Rodolfo Gonçalves" },
+  { id: "5", name: "Rodolfo Gonçalves" },
+  { id: "6", name: "Rodolfo Gonçalves" },
+  { id: "7", name: "Rodolfo Gonçalves" },
+  { id: "8", name: "Rodolfo Gonçalves" },
+  { id: "9", name: "Rodolfo Gonçalves" },
+];
 
 export function Participants() {
+  const theme = useTheme();
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <Container>
+      <Highlight
+        title="Nome do evento"
+        subtitle="Sexta, 4 de Novembro de 2022."
+      />
+      <WrapperInput>
+        <InputText placeholder="Nome do participante" />
+        <Button variant="GREEN_900">
+          <MaterialIcons name="add" size={24} color={theme.color.WHITE} />
+        </Button>
+      </WrapperInput>
+      <TitleContainer>
+        <Title>Participantes</Title>
+      </TitleContainer>
+      <FlatList
+        data={participants}
+        keyExtractor={({ id }) => id}
+        renderItem={({ item }) => <ListItem title={item.name} />}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={
+          <ListEmpty
+            text="Ninguém chegou no evento ainda? 
+Adicione participantes a sua lista de presença."
+          />
+        }
+      />
+    </Container>
   );
 }
---
+++ src/screen/Participants/styles.ts
@@ -0,0 +1,25 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import styled from "styled-components/native";
+
+export const Container = styled(SafeAreaView)`
+  flex: 1;
+  background-color: ${({ theme }) => theme.color.GRAY_400};
+  padding: 0 24px;
+`;
+
+export const WrapperInput = styled.View`
+  flex-direction: row;
+  align-self: stretch;
+  gap: 7px;
+  margin: 34px 0 0 0;
+`;
+
+export const TitleContainer = styled.View`
+  margin: 42px 0 16px 0;
+`;
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.color.GRAY_100};
+  font-family: ${({ theme }) => theme.fontFamily.ROBOTO_BOLD};
+  font-size: ${({ theme }) => theme.fontSize.MD};
+`;
