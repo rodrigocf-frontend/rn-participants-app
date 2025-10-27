973f10369f82feee8bbe87e0d0efc7b635045995
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2025-10-27 20:03:57 -0300

    feat: create application routes routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,28 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createStaticNavigation } from "@react-navigation/native";
 import { Home } from "../screen/Home";
 import { Participants } from "../screen/Participants";
-import { createStaticNavigation } from "@react-navigation/native";
+import { theme } from "../themes";
 
 const RootStack = createNativeStackNavigator({
   screens: {
-    Home: Home,
-    Participants: Participants,
+    Home: {
+      screen: Home,
+      options: {
+        headerShown: false,
+      },
+    },
+    Participants: {
+      options: {
+        headerShadowVisible: false,
+        headerTintColor: theme.color.GRAY_100,
+        headerStyle: {
+          backgroundColor: theme.color.GRAY_400,
+        },
+      },
+      screen: Participants,
+    },
   },
-
-  screenOptions: {},
 });
 
 export const Navigation = createStaticNavigation(RootStack);
