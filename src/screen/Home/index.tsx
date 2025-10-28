5c44bc0e3ba13d7e11a132453c618edb06b4564c
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2025-10-27 22:36:33 -0300

    feat: add  context and events and remove participants functions

--- src/screen/Home/index.tsx
+++ src/screen/Home/index.tsx
@@ -11,57 +11,92 @@ import {
   TitleContainer,
   WrapperInput,
 } from "./styles";
-import { FlatList } from "react-native";
+import { Alert, FlatList } from "react-native";
 import { InputDate } from "../../components/InputDate";
 import { useNavigation } from "@react-navigation/native";
 import { ListEmpty } from "../../components/ListEmpty";
-
-type Event = {
-  id: string;
-  name: string;
-  date: string;
-};
-
-const participants: Event[] = [
-  // { id: "1", name: "Evento 1", date: "14/02/2025" },
-  // { id: "2", name: "Evento 1", date: "14/02/2025" },
-  // { id: "3", name: "Evento 1", date: "14/02/2025" },
-  // { id: "4", name: "Evento 1", date: "14/02/2025" },
-  // { id: "5", name: "Evento 1", date: "14/02/2025" },
-  // { id: "6", name: "Evento 1", date: "14/02/2025" },
-  // { id: "7", name: "Evento 1", date: "14/02/2025" },
-  // { id: "8", name: "Evento 1", date: "14/02/2025" },
-  // { id: "9", name: "Evento 1", date: "14/02/2025" },
-];
+import { useContext, useState } from "react";
+import { EventType, EventsContext } from "../../store/EventsProvider";
+import dayjs, { Dayjs } from "dayjs";
+import { formatDate } from "../../utils/date-format";
 
 export function Home() {
   const theme = useTheme();
-
+  const { events, addEvent, removeEvent } = useContext(EventsContext);
   const navigation = useNavigation();
+  const [eventName, setEventName] = useState("");
+  const [eventDate, setEventDate] = useState<Dayjs>(dayjs());
+
+  const handleAddEvent = () => {
+    if (eventName.length <= 1) {
+      return Alert.alert(
+        "Nome de evento inválido",
+        "Nome de evento deve ter no mínimo 1 caracter."
+      );
+    }
+
+    return Alert.alert(
+      "Adcionar",
+      `Criar evento ${eventName}, no dia ${eventDate.format("DD/MM/YYYY")}?`,
+      [
+        {
+          text: "Sim",
+          onPress: () => {
+            addEvent({
+              date: formatDate(eventDate).format("DD/MM/YYYY"),
+              name: eventName.trim(),
+            });
+            setEventName("");
+            setEventDate(dayjs());
+          },
+        },
+        {
+          text: "Cancelar",
+          style: "cancel",
+        },
+      ]
+    );
+  };
+
+  const handleDeleteEvent = (event: EventType) => removeEvent(event.id);
 
   return (
     <Container>
-      <Highlight title="Cadastrar evento" />
+      <Highlight
+        title="Cadastrar evento"
+        subtitle={formatDate(dayjs()).format("DD/MM/YYYY")}
+      />
       <WrapperInput>
-        <InputText placeholder="Nome do evento" />
-        <Button variant="GREEN_900">
+        <InputText
+          placeholder="Nome do evento"
+          onChangeText={setEventName}
+          value={eventName}
+        />
+        <Button variant="GREEN_900" onPress={handleAddEvent}>
           <MaterialIcons name="add" size={24} color={theme.color.WHITE} />
         </Button>
       </WrapperInput>
       <DateContainer>
-        <InputDate />
+        <InputDate onChange={setEventDate} value={eventDate} />
       </DateContainer>
       <TitleContainer>
         <Title>Eventos</Title>
       </TitleContainer>
       <FlatList
-        data={participants}
+        data={events}
         keyExtractor={({ id }) => id}
         renderItem={({ item }) => (
           <ListItem
-            onPress={() => navigation.navigate("Participants")}
+            onPress={() =>
+              navigation.navigate("Participants", {
+                date: item.date,
+                id: item.id,
+                name: item.name,
+              })
+            }
             title={item.name}
             subtitle={item.date}
+            onRemove={() => handleDeleteEvent(item)}
           />
         )}
         showsVerticalScrollIndicator={false}
