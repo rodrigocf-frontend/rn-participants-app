e01f675207a11f2954b2b66e0e43f1a56b313c4a
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2025-10-28 10:34:45 -0300

    feat: change dependence library and format date on inputdate component

--- src/components/InputDate/index.tsx
+++ src/components/InputDate/index.tsx
@@ -4,23 +4,23 @@ import {
 } from "@react-native-community/datetimepicker";
 import { useEffect, useState } from "react";
 import { Value, Button, Container } from "./styles";
-import dayjs, { Dayjs } from "dayjs";
-import { formatDate } from "../../utils/date-format";
+import { formatTZDateToLocale } from "../../utils/date-format";
 import { MaterialIcons } from "@expo/vector-icons";
 import { useTheme } from "styled-components/native";
+import { TZDate } from "@date-fns/tz";
 
 interface Props {
-  onChange: React.Dispatch<React.SetStateAction<dayjs.Dayjs>>;
-  value: Dayjs;
+  onChange: React.Dispatch<React.SetStateAction<TZDate>>;
+  value: TZDate;
 }
 
 export function InputDate({ onChange, value }: Props) {
   const theme = useTheme();
-  const [date, setDate] = useState(formatDate(value));
+  const [date, setDate] = useState(value);
 
   const handleShow = () => {
     DateTimePickerAndroid.open({
-      value: date.toDate(),
+      value: date,
       onChange: handleChange,
       mode: "date",
       is24Hour: true,
@@ -29,20 +29,20 @@ export function InputDate({ onChange, value }: Props) {
 
   const handleChange = (_: DateTimePickerEvent, date?: Date) => {
     if (date) {
-      const utcDate = formatDate(dayjs(date));
+      const utcDate = new TZDate(date);
       onChange(utcDate);
+      setDate(utcDate);
     }
   };
 
   useEffect(() => {
-    const utcDate = formatDate(dayjs(value));
-    setDate(utcDate);
+    setDate(value);
   }, [value]);
 
   return (
     <Container>
       <Button onPress={handleShow}>
-        <Value>{date.format("DD/MM/YYYY")}</Value>
+        <Value>{formatTZDateToLocale(date)}</Value>
         <MaterialIcons
           size={24}
           name="calendar-month"
--- src/utils/date-format.ts
+++ src/utils/date-format.ts
@@ -1,3 +1,14 @@
-import { Dayjs } from "dayjs";
+import { TZDate } from "@date-fns/tz";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
-export const formatDate = (date: Dayjs) => date.locale("br");
+export const formatNumberToLocale = (date: number) => {
+  return format(new TZDate(date), "eee, dd 'de' MMMM 'de' yyyy.", {
+    locale: ptBR,
+  });
+};
+
+export const formatTZDateToLocale = (date: TZDate) =>
+  format(date, "dd/MM/yyyy", {
+    locale: ptBR,
+  });
